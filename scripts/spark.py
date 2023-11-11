36ef4e85d92be07699bb419f6d245fdac4ca79be
yezhan <zhan.ye@edu.ece.fr>
2023-11-11 17:34:26 +0100

    修改：     scripts/spark.py

--- scripts/spark.py
+++ scripts/spark.py
@@ -1,25 +1,40 @@
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
 from pyspark.sql.types import IntegerType, FloatType, StringType, DateType, TimestampType
+from pyspark import SparkContext
+from pyspark.ml.feature import VectorAssembler as va
+from pyspark.ml.regression import LinearRegression as linreg
+from pyspark.ml import Pipeline
 
-
+'''1. Ingérer les données en tant que pyspark dataframe object'''
 if __name__ == '__main__':
 # Créer une session Spark
-    spark = SparkSession.builder.\
-        appName('tp').\
-        getOrCreate()
-    sc = spark.sparkContext
-    
+    sc = SparkContext("local", "airports")
+    spark = SparkSession(sc)
 # Charger le jeu de données csv 
-    df_air = spark.read.csv('./data/airports.csv', sep=',', header=True)
-    df_fli = spark.read.csv('./data/flights.csv', sep=',', header=True)
-    df_raw=spark.read.csv('./data/raw-flight-data.csv', sep=',', header=True)
-    
+    df_air = spark.read \
+    .option('header', 'True') \
+    .option('delimiter', ',') \
+    .option('inferSchema', 'True') \
+    .csv('./data/airports.csv')
+    df_fli = spark.read \
+    .option('header', 'True') \
+    .option('delimiter', ',') \
+    .option('inferSchema', 'True') \
+    .csv('./data/flights.csv')
+    df_raw = spark.read \
+    .option('header', 'True') \
+    .option('delimiter', ',') \
+    .option('inferSchema', 'True') \
+    .csv('./data/raw-flight-data.csv')
+
 # Afficher le schéma du dataframe
 def dayingbiao():
     return df_air.show(5),df_fli.show(5),df_raw.show(5)
 #dayingbiao()
 
+
+'''2. Effectuer des analyses des données tout en utilisant les opérations à disposition'''
 # Afficher le nombre d'enregistrements
 def dayingshuliang():
     return  print("aéroports: ", df_air.count()),\
@@ -29,7 +44,6 @@ def dayingshuliang():
 
 # Afficher les colonnes du dataframe
 def dayingbiaotou():
-    
     return print("airports dataset: ", df_air.columns),\
             print("lights dataset: ", df_fli.columns),\
             print("raw-flight-data dataset: ", df_raw.columns)
@@ -60,7 +74,6 @@ def zhunshi(zhonglei):
 
     else:
         return "'dep', 'arr', ou 'all'？"
-
 #print(zhunshi('dep'))
 #print(zhunshi('arr'))
 #print(zhunshi('all'))
@@ -73,7 +86,7 @@ def pingjunzhi(df):
     df1=df.withColumn('totaldelay',F.col('DepDelay')+F.col('ArrDelay'))
     avg3=df1.select(F.avg('totaldelay')).collect()[0][0]
     return avg1,avg2,avg3
-print(pingjunzhi(df_fli))
+#print(pingjunzhi(df_fli))
 
 def hangban_info(start_airport_id, end_airport_id):
     """Cette fonction prend deux identifiants d'aéroport en entrée et renvoie toutes les informations de vol entre ces deux aéroports.
@@ -100,6 +113,9 @@ def diqv_info(location_type):
     
 #diqv_info().show()
 
+
+'''3. Evaluer la qualité des données et la préparation nécessaires pour qu’elles soient le mieux 
+exploitables (ex : valeurs manquantes, valeurs dupliquées.)'''
 # Afficher les aéroports qui ont le plus de vols
 def queshizhi(df):
     queshi_cols = []
@@ -124,7 +140,6 @@ df_raw=shanchuqueshizhi(df_raw)
 df_fli=shanchuqueshizhi(df_fli)
 df_air=shanchuqueshizhi(df_air)
 
-
 # Supprimer les doublons
 df_raw.dropDuplicates()
 df_fli.dropDuplicates()
@@ -135,7 +150,6 @@ def print_dtypes(df):
     for column, dtype in df.dtypes:
         print(f'{column}: {dtype}')
         
-
 #Convertir le type de données
 #print_dtypes(df_air)
 df_air = df_air.withColumn('airport_id', df_air['airport_id'].cast(IntegerType()))
@@ -153,3 +167,48 @@ for i in df_raw.columns:
         df_raw = df_raw.withColumn(i, df_raw[i].cast(IntegerType()))
 #print_dtypes(df_raw)
 
+
+
+'''import matplotlib.pyplot as plt
+import seaborn as sns
+
+# 将Spark DataFrame转换为Pandas DataFrame
+pandas_df = df_fli.toPandas()
+
+# 使用Seaborn的distplot方法来绘制目标变量的分布图
+sns.distplot(pandas_df['DepDelay'])
+plt.show()
+
+# 使用Seaborn的scatterplot方法来绘制目标变量和其他特征之间的散点图
+for column in pandas_df.columns:
+    if column != 'DepDelay':
+        sns.scatterplot(x='DepDelay', y=column, data=pandas_df)
+        plt.show()
+'''
+
+
+'''4. Concevoir un modèle de Machine Learning, en utilisant sparkML qui vous permettra de 
+prédire si un vol a eu du retard ou pas (entrainement + Test)'''
+#régression linéaire
+def xianxinghuigui(df,f_col,labcol):
+    '''Modèle de régression linéaire
+    :param f_col: colonne de caractéristiques
+    :param labcol: colonne d'étiquettes
+    :return: None
+    '''
+    a = va(inputCols=f_col, outputCol='features')
+    df= a.transform(df)
+    train, test = df.randomSplit([0.8, 0.2])
+    lr= linreg(featuresCol='features', labelCol=labcol)
+    lr_mod = lr.fit(train)
+    lr_eva = lr_mod.evaluate(test)
+    lr_eva_r = lr_eva.rootMeanSquaredError
+    print('他妈的值是'+str(lr_eva_r))
+    predictions = lr_mod.transform(test)
+    return predictions
+
+#sans aéroport
+col = df_fli.columns
+xianxinghuigui(df_fli,[c for c in col if c not in ['Carrier','OriginAirportID','DestAirportID']],'ArrDelay')
+#que DepDelay
+xianxinghuigui(df_fli,['DepDelay'],'ArrDelay').show()
